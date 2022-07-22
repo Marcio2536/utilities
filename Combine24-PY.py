9e4d644ca9b67606cb84a21d676307e7535c1c8a
Edmond <61924136+Marcio2536@users.noreply.github.com>
2022-07-22 23:06:58 +0800

    Add files via upload

--- Combine24-PY.py
+++ Combine24-PY.py
@@ -1,8 +1,10 @@
 # Original Repo: https://github.com/MCL7D9/RoBoMaster
 from ast import Pass
 from itertools import permutations
-import  time
-enter=input("Enter 4 number and a sign: ")
+import time
+import random
+import re
+enter=input("Enter a sign and 4 numbers: ")
 startime = time.time()
 sign=enter[4]
 ele=[0,0,0,0]
@@ -20,11 +22,9 @@ def analyze():
 	for i in range(4):
 		for j in range(4):
 			if i != j:
-				enter_list.append(str(enter_list[i])+str(enter_list[j]))
+				enter_list.append(int(str(enter_list[i])+str(enter_list[j])))
 				ddg_list.append(str(enter_list[i])+str(enter_list[j]))
 	length = len(enter_list)
-	for i in range(length):
-		enter_list[i]=int(enter_list[i])
 	if sign=="+":
 		pass
 		plus()
@@ -39,10 +39,15 @@ def analyze():
 		divid()
 def plus():
 	global ele, matched
+	tempstr = str(ele[0]) + str(ele[1]) + str(ele[2]) + str(ele[3])
 	a = int(ele[0])
 	b = int(ele[1])
 	c = int(ele[2])
 	d = int(ele[3])
+	if ele == [2,2,2,2]:
+		print('2 + 22 = 24')
+		matched = True
+	print(enter_list)
 	plist=enter_list
 	length=len(plist)
 	plist.sort(reverse=True)
@@ -64,95 +69,71 @@ def plus():
 	if not matched and b+c+d == 24:
 		print(b,'+', c, '+', d,'= 24')
 		matched = True
-	# AB + C + D
+	# Others 
+	
+	# AB + C + D !! Must be placed in last part of the function !!
 	if not matched:
-		temp = str(ele[0]) + str(ele[1]) + str(ele[2]) + str(ele[3])
-		templist = list(p for p in permutations(temp, 2))
-		templist = [item for t in templist for item in t]
-		for i in range(len(templist)):
-			templist[i] = int(templist[i])
-		for i in range(12):
-			tempsum = templist[2*i]*10 + templist[2*i + 1]
-			tempele = ele
-			try:
-				for k in range(4):
-					if templist[2*i] == ele[k]:
-						tempele.pop(k)
-			except:
-				pass
-			try:
-				for k in range(len(tempele)):
-					if templist[2*i + 1] == ele[k]:
-						tempele.pop(k)
-			except:
-				pass
-			tempsum2 = 0
-			for k in range(len(tempele)):
-				tempsum2 += tempele[k]
-			if tempsum + tempsum2 == 24:
-				print(tempsum,'+',tempele[0],'+',tempele[1],'= 24')
-				matched = True
+		opo_list = [0,0,0,0]
+		print(opo_list[3],opo_list[0])
+		for i in range(160):
+			opo_list = [ele[0],ele[1],ele[2],ele[3]]
+			r1 = random.randint(0,3)
+			a1 = opo_list[r1]
+			opo_list.pop(r1)
+			r2 = random.randint(0,2)
+			a2 = opo_list[r2]
+			opo_list.pop(r2)
+			sum1 = a1 *10 + a2
+			r3 = random.randint(0,1)
+			a3 = opo_list[r3]
+			opo_list.pop(r3)
+			a4 = opo_list[0]
+			if sum1 + a3 + a4 == 24:
+				print(sum1,'+',a3,'+',a4,'= 24')
 				break
-	while not matched:
-		if plist[0]>24:
-			del plist[0]
-		else:
-			matched = True
-		solution = ""
-		for i in range(len(plist)):
-			for j in range(len(plist)):
-				reslt=plist[i]+plist[j]
-				if reslt==24:
-						solution= str(plist[i]) + ' + ' + str(plist[j]) +' = 24'
-						break
-		print(solution)
+				matched = True
+			
 def minus():
 	global matched
 	mlist=enter_list
 	length=len(mlist)
 	mlist.sort(reverse=True)
 	i = 0
-	solution = ""
-	# AB - C - D
+	solution = "no solution"
+	# Others (Have to be re-done)
+	#for i in range(length):
+	#		for j in range(length):
+	#			if mlist[i] > mlist[j]:
+	#				reslt=mlist[i] - mlist[j]
+	#				if reslt==24:
+	#					solution= str(mlist[i]) + ' - ' + str(mlist[j]) +' = 24'
+	#					matched = True
+	#					break
+	#print(solution)
+	# AB - C - D !! Must be placed in the last part of the function !!
 	if not matched:
-		temp = str(ele[0]) + str(ele[1]) + str(ele[2]) + str(ele[3])
-		templist = list(p for p in permutations(temp, 2))
-		templist = [item for t in templist for item in t]
-		for i in range(len(templist)):
-			templist[i] = int(templist[i])
-		for i in range(12):
-			tempsum = templist[2*i]*10 + templist[2*i + 1]
-			tempele = ele
-			try:
-				for k in range(4):
-					if templist[2*i] == ele[k]:
-						tempele.pop(k)
-			except:
-				pass
-			try:
-				for k in range(len(tempele)):
-					if templist[2*i + 1] == ele[k]:
-						tempele.pop(k)
-			except:
-				pass
-			tempsum2 = 0
-			for k in range(len(tempele)):
-				tempsum2 += tempele[k]
-			if tempsum - tempsum2 == 24:
-				print(tempsum,'-',tempele[0],'-',tempele[1],'= 24')
-				matched = True
+		opo_list = [0,0,0,0]
+		print(opo_list[3],opo_list[0])
+		for i in range(240):
+			opo_list = [ele[0],ele[1],ele[2],ele[3]]
+			print(opo_list,i)
+			r1 = random.randint(0,3)
+			a1 = opo_list[r1]
+			opo_list.pop(r1)
+			r2 = random.randint(0,2)
+			a2 = opo_list[r2]
+			opo_list.pop(r2)
+			sum1 = a1 *10 + a2
+			r3 = random.randint(0,1)
+			a3 = opo_list[r3]
+			opo_list.pop(r3)
+			a4 = opo_list[0]
+			if sum1 - a3 - a4 == 24:
+				print(sum1,'-',a3,'-',a4,'= 24')
 				break
-	
-	# A - B
-	if not matched:
-		for i in range(length):
-			for j in range(length):
-				if mlist[i] > mlist[j]:
-					reslt=mlist[i] - mlist[j]
-					if reslt==24:
-						solution= str(mlist[i]) + ' - ' + str(mlist[j]) +' = 24'
-						break
-		print(solution)
+				matched = True
+
+		
 def multi():
 	global ele
 	a = int(ele[0])
