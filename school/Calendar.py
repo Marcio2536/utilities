763e6a9a660d1549bdb7fd0d1b8fff1a78e5389b
Edmond <61924136+Marcio2536@users.noreply.github.com>
2022-10-02 11:52:34 +0800

    Add files via upload

--- school/Calendar.py
+++ school/Calendar.py
@@ -4,18 +4,18 @@ def readconfig():
 	global exceptdate,skiprange,startdate
 	with open('config.yaml', 'r') as file:
 		yml = yaml.safe_load(file)
-	exceptls = yml['exceptdate']
+	startdate = yml['startdate']
+	exceptls = yml['exceptdate'] # Retrieve the dates that are going to be excluded
 	exceptdate = []
 	for i in range(len(exceptls)):
 		yyyy, mm, dd = exceptls[i].split('/')
 		exceptdate.append([int(yyyy),int(mm),int(dd)])
-	rangels = yml['skiprange']
+	rangels = yml['skiprange'] # Retrieve the range of dates that are going to be ignored
 	skiprange = []
 	for i in range(len(rangels)):
 		yyyy, mm, dd, dayofrange = rangels[i].split('/')
 		skiprange.append([int(yyyy),int(mm),int(dd),int(dayofrange)])
-	startdate = yml['startdate']
-def writeics(py,pm,pd,ey,em,ed,schday):
+def appendics(py,pm,pd,ey,em,ed,schday): # Append the corresponding date and school day to the calendar file
 	global ics
 	context = 'BEGIN:VEVENT\n'
 	context += 'DTSTART;VALUE=DATE:' + py + pm + pd + '\nDTEND;VALUE=DATE:' + ey + em + ed + '\nTRANSP:TRANSPARENT\nSUMMARY:Day ' + str(schday) + '\nEND:VEVENT\n'
@@ -27,13 +27,13 @@ def skipday(year,dayofyr):
 		if tempdate == givendate:
 			return skiprange[i][3]
 	return 1
-def checkweek(year,dayofyr):
+def checkweek(year,dayofyr): # Check if the date passed by the main program is weekend or not
 	tempdate = datetime.datetime(year, 1, 1) + datetime.timedelta(dayofyr - 1)
 	if (int(tempdate.strftime("%u")) == 6)or (int(tempdate.strftime("%u")) == 7):
 		return False
 	else:
 		return True
-def exception(year,dayofyr):
+def exception(year,dayofyr): # Check if the date passed by the main program matches the records on the config file or not  
 	global exceptdate
 	givendate = datetime.datetime(year, 1, 1) + datetime.timedelta(dayofyr - 1)
 	for i in range(len(exceptdate)):
@@ -43,17 +43,17 @@ def exception(year,dayofyr):
 	return False
 def main():
 	global initdate, ics, exceptdate
-	schtuple = (1,2,3,4,5,6)
+	schtuple = (1,2,3,4,5,6) # Set up a tuple for school day iteration
 	schday = iter(schtuple)
-	ics = open('schday.ics','at')
-	readconfig()
-	yyyy, mm, dd = map(int,startdate.split('/'))	
+	ics = open('schday.ics','at') 
+	readconfig() # Retrieve configuration from config.yaml
+	yyyy, mm, dd = map(int,startdate.split('/'))	# Retrieve the first record from configuration
 	ics.write('BEGIN:VCALENDAR\nPRODID:-//Pythonista\nVERSION:2.0\nCALSCALE:GREGORIAN\nX-WR-TIMEZONE:Asia/Hong_Kong\nX-WR-CALNAME:School Day\nX-WR-CALDESC:School Day\n')
 	initdate = datetime.datetime(yyyy,mm,dd)
 	currentyr = datetime.datetime(yyyy,12,31)
 	endmonth = datetime.datetime(yyyy+1,8,31)
-	daynum = int(initdate.strftime("%j"))
-	yearofday = int(currentyr.strftime("%j"))
+	daynum = int(initdate.strftime("%j")) # Turn the start date to day number of a year (1-366)
+	yearofday = int(currentyr.strftime("%j")) # Determine the number of days in a year (i.e. 365 Days for 2023, 366 Days for 2024)
 	endsummer = (int(endmonth.strftime("%j"))-1) if (yyyy+1) % 4 == 0 else int(endmonth.strftime("%j"))
 	i = daynum
 	while  i != endsummer:
@@ -61,14 +61,14 @@ def main():
 			printdate = datetime.datetime(yyyy, 1, 1) + datetime.timedelta(i - 1)
 			endate = printdate.date() + datetime.timedelta(days=1)		
 			i += 1
-			try:					
-				writeics(printdate.strftime('%Y'),printdate.strftime('%m'),printdate.strftime('%d'),endate.strftime('%Y'),endate.strftime('%m'),endate.strftime('%d'),next(schday))
+			try:	# Append the corresponding school day to the calendar file and reset the tuple iteration if StopIteration Error was prompt 		
+				appendics(printdate.strftime('%Y'),printdate.strftime('%m'),printdate.strftime('%d'),endate.strftime('%Y'),endate.strftime('%m'),endate.strftime('%d'),next(schday))
 			except:
 				schday = iter(schtuple)
-				writeics(printdate.strftime('%Y'),printdate.strftime('%m'),printdate.strftime('%d'),endate.strftime('%Y'),endate.strftime('%m'),endate.strftime('%d'),next(schday))
+				appendics(printdate.strftime('%Y'),printdate.strftime('%m'),printdate.strftime('%d'),endate.strftime('%Y'),endate.strftime('%m'),endate.strftime('%d'),next(schday))
 		else:
 			i += skipday(yyyy,i)
-		if i >= yearofday:
+		if i >= yearofday: # Proceed to the next year if the index is bigger than the number of days in a year 
 			yyyy += 1
 			i -= yearofday
 	ics.write('END:VCALENDAR')
